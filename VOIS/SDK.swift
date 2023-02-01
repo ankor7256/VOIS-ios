2513fa88b4f40369da06f7b53fc0f4b0604dc6e2
Коротков Андрей <andrey.korotkov@iw-group.pro>
2023-02-01 14:48:12 -0500

    Initial app

---
+++ VOIS/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  VOIS
+//
+//  Created by Andrew K on 01.02.23.
+//
+
+import Foundation
+
+final class SDK {
+    static func getRandom() -> Int {
+        return Int.random(in: 0...100)
+    }
+}
--- VOIS/ViewController.swift
+++ VOIS/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet private var valueLabel: UILabel!
+    @IBOutlet private var getButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
         // Do any additional setup after loading the view.
     }
+    
+    private func configure() {
+        valueLabel.text = String(SDK.getRandom())
+        getButton.addTarget(self, action: #selector(getButtonClick(_:)), for: .touchUpInside)
+    }
 
-
+    @objc private func getButtonClick(_ sender : UIButton){
+        valueLabel.text = String(SDK.getRandom())
+    }
 }
 
